812ae67c4d40b8e657aa670fe88ac7062e8f6774
Pierre Marquet <marquet_pierre@yahoo.fr>
2021-07-30 14:43:13 +0200

    update

--- libraries/simpleSlide/script.js
+++ libraries/simpleSlide/script.js
@@ -2,13 +2,18 @@
 
 let x = 0;
 let dist = 200;
+let max = 600;
 
 $("#nextButton").click(function () {
     x = x + dist;
+    if (x > max) { x = max }
     $("#customersSlide").animate({ scrollLeft: x }, 400);
+    console.log(x);
 });
 
 $("#previousButton").click(function () {
     x = x - dist;
+    if (x < 0) { x = 0 }
     $("#customersSlide").animate({ scrollLeft: x }, 400);
+    console.log(x);
 });
