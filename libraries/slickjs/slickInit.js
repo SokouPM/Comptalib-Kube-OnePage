dcc7a656024bcde1ad2dfe718ae71debf0ebc7d2
Pierre Marquet <marquet_pierre@yahoo.fr>
2021-08-03 16:21:35 +0200

    update

--- libraries/slickjs/slickInit.js
+++ libraries/slickjs/slickInit.js
@@ -1,18 +1,33 @@
-$(document).ready(function(){
-    $('.productionSlide').slick({
-        infinite: true,
-        slidesToShow: 3,
-        slidesToScroll: 1,
-        autoplay: true,
-        autoplaySpeed: 2000,
-        responsive: [
-            {
-              breakpoint: 940,
-              settings: {
-                slidesToShow: 1,
-                arrows: false,
-              }
-            }
-          ]
-    });
+$(document).ready(function () {
+  $('.productionSlide').slick({
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    responsive: [
+      {
+        breakpoint: 940,
+        settings: {
+          slidesToShow: 1,
+          arrows: false,
+        }
+      }
+    ]
   });
+
+  $('.customerSlide').slick({
+    infinite: true,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 940,
+        settings: {
+          slidesToShow: 1,
+          arrows: false,
+        }
+      }
+    ]
+  });
+});
