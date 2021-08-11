8adcae533939cf35bbb853c12fddf951430cd67e
Pierre Marquet <marquet_pierre@yahoo.fr>
2021-08-11 12:43:53 +0200

    update

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
 $(document).ready(function () {
     $('#ScrollEngagements').on('click', function (e) {
         e.preventDefault();
-        let page = $(this).attr('href');
-        $('html, body').animate({ scrollTop: $(page).offset().top }, 800);
+        let findOutMoreDest = $(this).attr('href');
+        $('html, body').animate({ scrollTop: $(findOutMoreDest).offset().top }, 800);
     });
+});
+
+$(document).ready(function () { // To center e-commerce pack
+    let packWidth =  $('.pack').width();
+    let viewWidth =  $(window).width();
+    let centerDim = (viewWidth - packWidth) / 2;
+    $('.packsSlide').animate({ scrollLeft: $('#ecommercePack').offset().left - centerDim }, 1);
 });
